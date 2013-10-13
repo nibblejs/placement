6c0643279abaaadc5f2fb06f0bf572bbb1cd82d2
Priyank <priyankmishra1102@gmail.com>
2013-10-13 23:20:25 +0530

    Added by Priyank Mishra

--- scripts/MySqlDB.php
+++ scripts/MySqlDB.php
@@ -7,14 +7,14 @@ class MySqlDB {
 
     public function __construct($host, $username, $password, $db)
     {
-        $this->_mysql = new mysqli($host, $username, $password, $db);
+        $this->_mysql = new mysqli($host, $username, $password, $db) or die('Could not connect to database');
     }
 
     public function query($query)
     {
         $this->_query = filter_var($query, FILTER_SANITIZE_STRING);
 
-        $stmt = $this->_mysql->prepare($query);
+        $stmt = $this->_mysql->prepare($query) or die('Could not execute query');
         $stmt->execute();
     }
 
